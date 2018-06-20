bed56ce0836108ff2caaae6572cf2b04289e2e2b
ZoeCarter <zcarter-konate@micds.org>
2018-06-20 18:20:52 +0000

    automatically backed up by learn

---
+++ summer_olympics.rb
@@ -0,0 +1,12 @@
+places_the_summer_olympics_took_place = {
+  "1996" => "Atlanta",
+  "2000" => "Sydney",
+  "2004" => "Athens",
+  "2008" => "Beijing",
+  "2012" => "London",
+}
+
+places_the_summer_olympics_took_place.each do |year, places|
+  puts "the #{year} olympics took place at #{places}!!!"
+  puts ""
+end 
